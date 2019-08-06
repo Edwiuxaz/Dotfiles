57234d746a34d9299f2e7cf8e20e4f5e5d664665
Edvinas Steponavičius <Edvinas.Steponavicius@atea.lt>
2019-08-06 08:37:55 +0300

    Changed directory for Tasks.org

--- .emacs.rc/org-mode-rc.el
+++ .emacs.rc/org-mode-rc.el
@@ -1,7 +1,7 @@
 (global-set-key (kbd "C-x a") 'org-agenda)
 (global-set-key (kbd "C-c C-x j") #'org-clock-jump-to-current-clock)
 
-(setq org-agenda-files (list "~/Documents/Agenda/"))
+(setq org-agenda-files (list "~/Dropbox/Agenda/"))
 
 (setq org-export-backends '(md))
 
