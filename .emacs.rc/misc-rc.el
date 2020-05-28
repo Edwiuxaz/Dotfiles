dcad596bfa564aaf2cc87e8edbeaff738c92dae2
Edvinas Steponavičius <Edvinas.Steponavicius@atea.lt>
2020-05-28 16:23:34 +0300

    Changed style of the cursor

--- .emacs.rc/misc-rc.el
+++ .emacs.rc/misc-rc.el
@@ -1,6 +1,7 @@
 (setq-default inhibit-splash-screen t
 	      make-backup-files nil
 	      tab-width 4
+          cursor-type 'bar
 	      indent-tabs-mode nil
 	      compilation-scroll-output t
 	      default-input-method "lithuanian-keyboard"
